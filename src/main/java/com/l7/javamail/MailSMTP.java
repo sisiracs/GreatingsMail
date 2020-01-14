8ddd2c437eda9587e4b897ec1cda81f3353d0636
Anson-lab <anson@litmus7.com>
2020-01-14 12:54:23 +0530

    conflict

--- src/main/java/com/l7/javamail/MailSMTP.java
+++ src/main/java/com/l7/javamail/MailSMTP.java
@@ -14,7 +14,7 @@ import com.l7.dto.MailData;
 
 public class MailSMTP {
 	Session mailSession;
-
+kasgdkgsakdgaskdga
 	public void mailSent(MailData mailobj, String[] toEmail, String emailSubject, String emailBody)
 			throws AddressException, MessagingException {
 		MailSMTP javaEmail = new MailSMTP();
